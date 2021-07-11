37f4752f5e76399c68ee8e581b33ddb51f54ccad
60rzvvbj <1808078515@qq.com>
2021-07-11 21:18:59 +0800

    init app

---
+++ src/views/home/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h2>主页</h2>
+    <router-link to="/login">登录页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
---
+++ src/views/login/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>登录页</h2>
+    <router-link to="/home">主页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style>
+</style>
