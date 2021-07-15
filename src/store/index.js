12e61849da799c466584d1afa4993f82752f5d5f
賴TKRJ <1030970578@qq.com>
2021-07-15 17:42:15 +0800

    element组件库+login表单

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import {
+  Button,
+  Form,
+  FormItem,
+  Input
+} from 'element-ui'
 
 Vue.use(Vuex);
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
 
 export default new Vuex.Store({
   state: {},
