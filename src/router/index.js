858159b6413385b8aa646c0d430b2812f3cca146
賴TKRJ <1030970578@qq.com>
2021-07-17 17:23:11 +0800

    挂载路由导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -24,4 +24,18 @@ const router = new VueRouter({
   routes
 });
 
+
+// 挂载路由导航守卫
+// router.beforeEach((to, from, next) => {
+//   // to将要访问的路径
+//   // from代表从哪个路径的跳转而来
+//   // next是一个函数，表示放行
+//   // next() 放行 next('/login')强制跳转
+//   if (to.path === '/login') return next();
+//   // 获取token
+//   const tokenStr = window.sessionStorage.getItem('token');
+//   if (!tokenStr) return next('/login');
+//   next();
+// })
+
 export default router;
