0fe1dd6abb4ea65d96b88f7c3cdc8530e9e73374
賴TKRJ <1030970578@qq.com>
2021-07-23 17:35:10 +0800

    修改密码盒子

--- src/views/setting/Setting.vue
+++ src/views/setting/Setting.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="settiing">
+  <div class="setting">
     <Header></Header>
+    <div class="change_box"></div>
   </div>
 </template>
 
@@ -21,4 +22,21 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.setting {
+  width: 100%;
+  height: 100%;
+  background-color: #f0f4f7;
+}
+.change_box {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  width: 70%;
+  height: 60%;
+  transform: translate(-50%, -50%);
+  box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+    7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
+  border-radius: 20px;
+  background-color: #fff;
+}
 </style>
